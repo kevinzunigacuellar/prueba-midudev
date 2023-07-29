24acf1f8b3a667449c89b1cc69e6d6feba5e95a2
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-07-29 19:17:42 -0400

    style add to read list

--- src/components/BooksShowcase.tsx
+++ src/components/BooksShowcase.tsx
@@ -18,24 +18,27 @@ export function BookShowcase() {
         when={store.showcaseBooks.length > 0}
         fallback={<ShowcaseFallback />}
       >
-        <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <ul class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6">
           <For each={store.showcaseBooks}>
             {(book) => (
-              <li class="relative rounded-md overflow-hidden">
+              <li class="p-2">
                 <img
                   src={book.cover}
                   alt={book.title}
-                  class="h-full w-auto"
+                  class="w-full aspect-[2/3]"
                   loading="lazy"
                   decoding="async"
                 />
-                <div class="absolute inset-0 hover:bg-white/40 hover:backdrop-blur transition-opacity flex justify-center items-center flex-col p-2 text-center opacity-0 hover:opacity-100">
-                  <h2 class="font-bold">{book.title}</h2>
-                  <button
-                    class="bg-blue-700 text-white uppercase text-sm font-bold px-2 py-1 mt-2 rounded-md"
-                    onClick={() => addToReadingList(book)}
-                  >
-                    Agregar
+                <div class="pt-3 flex gap-2 justify-between items-start">
+                  <div class="">
+                    <p class="text-zinc-300">{book.title}</p>
+                    <p class="text-zinc-400">{book.author.name}</p>
+                  </div>
+                  <button class="p-1 mt-1 bg-zinc-700 rounded-md text-zinc-400 hover:bg-red-700 hover:text-zinc-300 transition-all" onclick={() => addToReadingList(book)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+</svg>
+
                   </button>
                 </div>
               </li>
